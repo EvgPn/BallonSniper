813cbbe35d8eabc01e375d7fba8e39ead775e446
Evg Pn <ye.ponomarenko@gmail.com>
2020-06-06 17:03:44 +0300

    feat(project) add palletes
    -palletes gets colors from List of Colors

---
+++ BallonSniper/Assets/Scripts/PalleteCollorsCreator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PalleteCollorsCreator : MonoBehaviour
+{
+	[SerializeField] private List<Color> _colors = new List<Color>();
+	public List<Color> GetColorsFromPalletes { get { return _colors; } }
+
+	[SerializeField] private List<GameObject> _palletesButtons = new List<GameObject>();
+
+	private void Start()
+	{
+		SetPalletesColors();
+	}
+
+	private void SetPalletesColors()
+	{
+		int counterForCollors = 0;
+
+		foreach (GameObject pallete in _palletesButtons)
+		{
+			pallete.GetComponent<Image>().color = _colors[counterForCollors];
+			counterForCollors++;
+		}
+	}
+}
